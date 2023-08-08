9ab2961d673504f994b7a174ed8fe0ca66fc2cd1
inklingboi <inklingboiii1@gmail.com>
2023-08-08 13:19:22 +0200

    added controls and green side

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { createCubeState, turnR, turnL, turnF, colorMap } from './cube-state';
 let cubeState = createCubeState();
 console.table(cubeState)
@@ -12,34 +13,47 @@ document.body.appendChild( renderer.domElement );
 let cube = createCube(cubeState);
 scene.add(cube);
 
-camera.position.z = 100;
+camera.position.z = 50;
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update()
 
 animate();
 
 function animate() {
 	requestAnimationFrame( animate );
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
 	renderer.render( scene, camera );
 }
 
-function createCube(state) {
-    let flattenedState = state.flat(3);
+function createCube(solvedState) {
+    let flattenedState = solvedState.flat(3);
     const count = flattenedState.length
-    console.log(count)
-    const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    const geometry = new THREE.PlaneGeometry( 1, 1, 1 );
     const material = new THREE.MeshBasicMaterial({color: 0xffffff});
     const cube = new THREE.InstancedMesh( geometry, material, count);
     // Used to position all instances with its .matrix
     const positionalObject = new THREE.Object3D();
     positionalObject.geometry = geometry;
-    for (let index = 0; index < count; index++) {
-        positionalObject.position.set(Math.random() * 50, Math.random() * 50, Math.random() * 50);
-        positionalObject.updateMatrix();
-        cube.setMatrixAt(index, positionalObject.matrix);
-        cube.setColorAt(index, new THREE.Color(colorMap[flattenedState[index]]));
+    for (let side = 0; side < solvedState.length; side++) {
+        // Flatten the rows
+        for (let cell = 0; cell < solvedState[0].flat().length; cell++) {
+            const index = side * solvedState[0].flat().length + cell;
+            const row = Math.floor(cell / 3);
+            const col = cell % 3;
+            positionalObject.position.set(Math.random() * 50, Math.random() * 50, Math.random() * 50);
+            positionalObject.rotation.x = 0;
+            if (side == 1) {
+                positionalObject.position.set(col,3 - row, 0);
+                positionalObject.rotateX(0)
+            } else if (side == 0) {
+                positionalObject.position.set(col, 3, -3 + row);
+                console.log('rotated')
+                positionalObject.rotateX(-90)
+            }
+            console.log(positionalObject.rotation.x)
+            positionalObject.updateMatrix();
+            cube.setMatrixAt(index, positionalObject.matrix);
+            cube.setColorAt(index, new THREE.Color(colorMap[solvedState[side][row][col]]));
+        }
     }
     cube.instanceColor.needsUpdate = true;
     return cube;
