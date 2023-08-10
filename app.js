7e6a1d8f6ca7da623c8a8e23fbd86b5921401cb9
inklingboi <inklingboiii1@gmail.com>
2023-08-10 13:01:03 +0200

    added white plane

--- app.js
+++ app.js
@@ -25,10 +25,11 @@ function animate() {
 }
 
 function createCube(solvedState) {
+    const stickerSize = 1;
     let flattenedState = solvedState.flat(3);
     const count = flattenedState.length
-    const geometry = new THREE.PlaneGeometry( 1, 1, 1 );
-    const material = new THREE.MeshBasicMaterial({color: 0xffffff});
+    const geometry = new THREE.PlaneGeometry(stickerSize, stickerSize, stickerSize);
+    const material = new THREE.MeshBasicMaterial({color: 0xffffff, side: THREE.DoubleSide});
     const cube = new THREE.InstancedMesh( geometry, material, count);
     // Used to position all instances with its .matrix
     const positionalObject = new THREE.Object3D();
@@ -37,17 +38,19 @@ function createCube(solvedState) {
         // Flatten the rows
         for (let cell = 0; cell < solvedState[0].flat().length; cell++) {
             const index = side * solvedState[0].flat().length + cell;
-            const row = Math.floor(cell / 3);
-            const col = cell % 3;
+            const rowLength = solvedState[0].length;
+            const row = Math.floor(cell / rowLength);
+            const col = cell % rowLength;
             positionalObject.position.set(Math.random() * 50, Math.random() * 50, Math.random() * 50);
             positionalObject.rotation.x = 0;
             if (side == 1) {
-                positionalObject.position.set(col,3 - row, 0);
+                positionalObject.position.set(col,rowLength - row, 0);
                 positionalObject.rotateX(0)
             } else if (side == 0) {
-                positionalObject.position.set(col, 3, -3 + row);
+                // Adjust the position due to the rotation
+                positionalObject.position.set(col, rowLength + stickerSize / 2, -rowLength + row + stickerSize / 2);
                 console.log('rotated')
-                positionalObject.rotateX(-90)
+                positionalObject.rotateX(Math.PI / 2)
             }
             console.log(positionalObject.rotation.x)
             positionalObject.updateMatrix();
@@ -56,5 +59,6 @@ function createCube(solvedState) {
         }
     }
     cube.instanceColor.needsUpdate = true;
+    console.log(cube)
     return cube;
 }
