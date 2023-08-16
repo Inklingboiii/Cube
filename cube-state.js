4ece9a56b3023c5276ca838f06d9f819c1961598
inklingboi <inklingboiii1@gmail.com>
2023-08-16 18:55:51 +0200

    added solving 4 moves wide

--- cube-state.js
+++ cube-state.js
@@ -43,6 +43,42 @@ function createCubeState() {
     ];
 }
 
+function createCrossState() {
+    return [
+        [
+            [-1, 0, -1],
+            [-1, 0, -1],
+            [-1, 0, -1]
+        ],
+        [
+            [-1, 1, -1],
+            [-1, 1, -1],
+            [-1, -1, -1]
+        ],
+        [
+            [-1, 2, -1],
+            [-1, 2, -1],
+            [-1, -1, -1]
+        ],
+        [
+            [-1, 3, -1],
+            [-1, 3, -1],
+            [-1, -1, -1]
+        ],
+        [
+            [-1, 4, -1],
+            [-1, 4, -1],
+            [-1, -1, -1]
+        ],
+        [
+            [-1, 5, -1],
+            [-1, 5, -1],
+            [-1, -1, -1]
+        ],
+        
+    ];
+}
+
 let colorMap = [0xffffff, 0x33FF33, 0xffdd33, 0x3344ff, 0xff0000, 0xff8833];
 
 function turnWithMap(state, translationMap, amount=1) {
@@ -60,7 +96,7 @@ function generateRandomState(state) {
     // https://stackoverflow.com/questions/2532218/pick-random-property-from-a-javascript-object
     let possibleTurns = Object.keys(turnMaps);
     let turnsList = [];
-    for (let i = 0; i < 2; i++) {
+    for (let i = 0; i < 4; i++) {
         let turnKey = possibleTurns[Math.floor(possibleTurns.length * Math.random())];
         let amount = Math.floor(Math.random() * 3) + 1;
         state = turnWithMap(state, turnMaps[turnKey], amount)
@@ -71,28 +107,48 @@ function generateRandomState(state) {
 }
 
 function solveState(state) {
-    let solvedState = createCubeState()
-    let statesQueue = new Set();
-    statesQueue.add(state);
-    let currentState = [...statesQueue][0]
+    let solvedState = createCubeState();
+    let statesQueue = [];
+    let resolvedQueue = []
+    resolvedQueue.push({turn: null,amount: 0, index: -1});
     let solvedStateFound = false;
-    for (let i = 0; i < 30; i++) {
+
+    for (const turnKey in turnMaps) {
+        for (let i = 0; i < 3; i++) {
+            statesQueue.push({turn: turnKey, amount: i+1, index: 0});
+        }
+    }
+    for (let i = 0; i < 100000; i++) {
         if(solvedStateFound) break;
-        currentState = [...statesQueue][0];
+        const referenceIndex = resolvedQueue.length;
         turnLoop:
         for (const turnKey in turnMaps) {
+            if(statesQueue[0].turn === turnKey) continue;
             for (let i = 0; i < 3; i++) {
-                let newState = turnWithMap(currentState, turnMaps[turnKey], i+1)
-                statesQueue.add(newState);
-                console.log(turnKey, i+1)
-               // console.table(newState)
-                if (JSON.stringify(newState) === JSON.stringify(solvedState)) {
+                let turnsList = []
+                let currentNode = statesQueue[0]
+                // Check if position is already om solved state before adding turns
+                while(true) {
+                    turnsList.push(currentNode);
+                    currentNode = resolvedQueue[currentNode.index];
+                    if(currentNode.index < 0) break;
+                }
+                let stateCopy = JSON.parse(JSON.stringify(state));
+                turnsList.reverse().map(node => {
+                 //   console.log(JSON.parse(JSON.stringify(stateCopy)), node)
+                    stateCopy = turnWithMap(stateCopy, turnMaps[node.turn], node.amount);
+                });
+                if (JSON.stringify(stateCopy) === JSON.stringify(solvedState)) {
                     console.log("found")
+                    console.log(JSON.parse(JSON.stringify(turnsList)));
+                    console.log(resolvedQueue)
                     solvedStateFound = true;
                     break turnLoop;
                 }
+                statesQueue.push({turn: turnKey, amount: i+1, index: referenceIndex});
             }
         }
+        resolvedQueue.push(statesQueue.shift())
     }
 }
 export {createCubeState, colorMap, turnWithMap, generateRandomState, solveState}
